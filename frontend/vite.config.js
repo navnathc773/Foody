49880a17e457962d5cc76728473f8949da521c7f
navnathc773 <navnathc773@gmail.com>
2025-09-03 11:21:56 +0530

    some new modifications

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "https://foodierest-production-b593.up.railway.app/",
+        target: "https://foodierest-production-b593.up.railway.app",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
+      "/specialapi": {
+        target: "https://foodierest-production-14ff.up.railway.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/specialapi/, ""),
+      },
     },
+    historyApiFallback: true, 
   },
-})
+});
